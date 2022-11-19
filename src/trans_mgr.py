0e8efcf758c6c0c6b234ee83512bd4d704871629
Anirudh Sriram <anirudh.sriram96@gmail.com>
2022-11-18 19:57:04 -0500

    Parse input file successfully using regex

--- src/trans_mgr.py
+++ src/trans_mgr.py
@@ -1,3 +1,5 @@
+import re
+
 class trans_mgr:
     def __init__(self):
         print("Creating transaction manager")
@@ -7,5 +9,22 @@ class trans_mgr:
         print("Committing transaction")
 
 if __name__ == "__main__":
-    print("Hello, world.")
+    # print("Hello, world.")
+    
     # TODO: Read from transaction set file, call TM on each operation
+    # Reading transactions from file
+    trans_set_file = open("trans-sets/set-1", "r")
+
+    # Regexes for comments, operations
+    comment_regex = re.compile(r'( )*#(.)*')
+    operation_regex = re.compile(r'(.+)\((.*)\)')
+
+    # Getting all lines
+    lines = trans_set_file.read().splitlines()
+
+    # Grouping up
+    for line in lines:
+        if(comment_regex.match(line)):
+            continue
+        elif(groups := operation_regex.match(line).groups()):
+            print(groups)
