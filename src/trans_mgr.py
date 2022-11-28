9c5457aa89c38f89d5f9de84b5f11faea8bf87ec
Anirudh Sriram <anirudh.sriram96@gmail.com>
2022-11-28 17:17:38 -0500

    Add cycle detection when adding to queue

--- src/trans_mgr.py
+++ src/trans_mgr.py
@@ -9,7 +9,7 @@ class trans_mgr:
         self.time = 0
         self.trans_set = {}
         self.vars = {}
-        self.wf_cycle = False
+        self.possible_cycle = False
 
         self.data_mgrs = [None] * 11        
         for mgr_idx in range(1, 11, 1):
@@ -59,9 +59,27 @@ class trans_mgr:
                 return False
         return True
 
+    def get_age(self, trans):
+        print("trans", trans)
+        if(trans in self.trans_set):
+            return self.time - self.trans_set[trans]["entry_time"]
+        return -1
+    
     def operate(self, op):
         print("TM: processing operation", op)
         self.time += 1
+
+        # Possible cycle from previous operation
+        if(self.possible_cycle):
+            print("TM: cycle possible")
+            (transs, is_cycle) = self.wf_graph.cycle_check()
+            print("TM: is there?", is_cycle, "transactions", str(transs))
+            if(is_cycle):
+                # Abort youngest transaction
+                transs_ages = list(map(lambda t: (t, self.get_age(t)), transs))
+                print("trans with ages", transs_ages)
+                pass
+            self.possible_cycle = False
 
         # Incoming transaction is a "begin"
         if(op["cmd"] == "begin" or op["cmd"] == "beginRO"):
@@ -120,13 +138,15 @@ class trans_mgr:
             if(has_lock):
                 print("TM: Transaction already has W lock")
                 self.write(trans, var, val)
+                return
 
             can_lock = True
             at_least_one_site = False
 
+            # Transaction doesn't already have lock, try to get lock
             for mgr_idx in range(1, 11, 1):
-                if((not mgrs[mgr_idx].mode == "failed") and # Site up
-                    self.is_at_mgr(var, mgr_idx)): # Site contains variable
+                if((not mgrs[mgr_idx].mode == "failed") # Site up
+                    and self.is_at_mgr(var, mgr_idx)): # Site contains variable
                     can_lock = can_lock and mgrs[mgr_idx].test_lock_var(trans, var, "write", to_lock = False)
                     at_least_one_site = True
 
@@ -140,14 +160,29 @@ class trans_mgr:
                         mgrs[mgr_idx].test_lock_var(trans, var, "write", to_lock = True)
                     print(mgr_idx, mgrs[mgr_idx].lock_table)
                 self.write(trans, var, val)
+                self.vars[var]["queue"].appendleft({
+                    "trans": trans,
+                    "lock": "write",
+                    "time": self.time
+                })
             else:
-                self.vars[var]["queue"].append({
+                self.vars[var]["queue"].appendleft({
                     "trans": trans,
                     "lock": "write",
                     "time": self.time
                 })
                 print("TM: Variable not available at any site or cannot lock, adding to queue", var, self.vars[var]["queue"])
-                print("TM: Adding var to waits-for graph")
+                print("TM: Adding transaction", trans, "to waits-for graph, waiting for")
+                queue = self.vars[var]["queue"]
+                if(len(queue) >= 1):
+                    dest = queue[1]["trans"]
+                    print("TM: Adding connection from", trans, "to", dest)
+                    self.wf_graph.add_edge(trans, dest)
+                    print("wf graph", self.wf_graph)
+                    self.possible_cycle = True
+                else:
+                    print("TM: No need to add anything to wf graph")
+                
                 
         elif(op["cmd"] == "R"):
             trans = op["args"][0]
@@ -170,20 +205,24 @@ class trans_mgr:
                             can_lock = mgrs[mgr_idx].test_lock_var(trans, var, "read", to_lock = False)
                             if(can_lock):
                                 break
-                else: # Transactions already in queue for item, so add this one too
-                    self.vars[var]["queue"].append({
-                        "trans": trans,
-                        "lock": "read",
-                        "time": self.time
-                    })
 
-                if(can_lock): # If locking possible, obtain read lock
+                # Add trans to var queue, raise possibility of cycle during next iteration
+                self.vars[var]["queue"].appendleft({
+                    "trans": trans,
+                    "lock": "read",
+                    "time": self.time
+                })
+                self.possible_cycle = True
+
+                if(can_lock): # If locking possible, obtain read lock, add item to queue
                     mgrs[mgr_idx].test_lock_var(trans, var, "read", to_lock = True)
+                    print("TM: Obtained lock at", mgr_idx, "adding item to queue for var")
+
             else: # Transaction is RO
                 var_idx = int(var[1:])
                 entry_time = self.trans_set[trans]["entry_time"]
                 print("TM: transaction is RO, fetching latest commit", var_idx)
-                if(var_idx%2 == 1): # The variable is not replicated
+                if(var_idx%2 == 1): # The variable is not replicated (maybe condition not necessary)
                     mgr = self.data_mgrs[(var_idx + 1) % 10]
                     if(mgr.mode == "failed"):
                         # Abort transaction
@@ -201,11 +240,9 @@ class trans_mgr:
                                 at_least_one_site = True
                                 break
                     if(not at_least_one_site):
+                        print("TM: fetch failed, aborting")
                         # abort transaction
                         pass
-
-
-
                         
             # TODO: Check if transaction is RO
             # TODO: If so, then refer (1)
@@ -232,8 +269,6 @@ class trans_mgr:
                 print(mgr_idx, self.data_mgrs[mgr_idx])
 
 
-
-
 # Create operation objects from groups
 def make_operation(groups):
     op = {
@@ -245,7 +280,7 @@ def make_operation(groups):
 
 if __name__ == "__main__":
     # Reading transactions from file
-    trans_set_file = open("trans-sets/set-3", "r")
+    trans_set_file = open("trans-sets/set-1", "r")
     tm = trans_mgr()
 
     # Regexes for comments, operations
